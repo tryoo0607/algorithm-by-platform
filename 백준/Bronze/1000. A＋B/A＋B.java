8231327c1e4cf7463df18da1234ea1ad9e0b7e2b
TeakRyeol Yoo <80720210+tryoo0607@users.noreply.github.com>
2025-07-07 17:58:30 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int int1 = scanner.nextInt();
+        int int2 = scanner.nextInt();
+        
+        System.out.println(int1 + int2);
+    }
+}

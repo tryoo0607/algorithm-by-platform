1f6dbb7b903b1cfce07f0f327aefd9274ffe0c30
TeakRyeol Yoo <80720210+tryoo0607@users.noreply.github.com>
2025-09-01 20:45:44 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 252 ms, Memory: 18076 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    
+        
+    for(int i = 0; i < n; i++) {
+        for(int j=0; j <=i; j++) {
+            System.out.print("*");
+        }
+        
+        System.out.println("");
+    }
+  }
+}

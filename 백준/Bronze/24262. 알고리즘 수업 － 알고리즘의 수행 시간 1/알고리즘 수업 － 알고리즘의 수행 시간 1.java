de3fe35b972165cc44bf737a1cd16588ca4dc9d9
TeakRyeol Yoo <80720210+tryoo0607@users.noreply.github.com>
2025-10-10 13:47:00 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+import java.lang.Math;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        System.out.println(1);
+        System.out.println(0);
+    }
+}

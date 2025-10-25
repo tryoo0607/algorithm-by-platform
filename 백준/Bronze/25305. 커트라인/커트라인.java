52c88454d0791341733887f2e7e604f4627ba990
TeakRyeol Yoo <80720210+tryoo0607@users.noreply.github.com>
2025-10-25 17:57:46 +0900

    [Bronze II] Title: 커트라인, Time: 220 ms, Memory: 20004 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+import java.util.stream.*;
+
+import java.lang.Math;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        int k = in.nextInt();
+        
+        List<Integer> arr = new ArrayList<>();
+
+        for(int i=0; i < n; i++) {
+            arr.add(in.nextInt());
+        }
+
+        arr.sort(Comparator.reverseOrder());
+
+        int cut = arr.get(k - 1);
+        
+        System.out.println(cut);
+    }
+
+} 
